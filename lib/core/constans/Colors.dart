bc8aa46c30baee9c976fa7f6a3aca7183b9ff5c7
AlexGoodD <alejandrochavarb@gmail.com>
2024-09-25 17:49:48 -0600

    Create paletteColors class

--- lib/core/constans/Colors.dart
+++ lib/core/constans/Colors.dart
@@ -7,3 +7,12 @@ class AppColors {
   static const routeSelected = Color.fromRGBO(35, 33, 138, 1.0);
   static const routeAlternative = Color.fromRGBO(35, 33, 138, 1.0);
 }
+
+class paletteColors {
+  static const firstColor = Color.fromRGBO(46, 48, 97, 1.0); //2e3061
+  static const secondColor = Color.fromRGBO(40, 41, 61, 1.0); //28293d
+  static const thirdColor = Color.fromRGBO(85, 81, 132, 1.0); //555184
+  static const fourthColor = Color.fromRGBO(153, 151, 188, 1.0); //9997bc
+  static const fifthColor = Color.fromRGBO(178, 166, 190, 1.0); //b3a6be
+  static const sixthColor = Color.fromRGBO(254, 233, 206, 1.0); //fee9ce
+}
