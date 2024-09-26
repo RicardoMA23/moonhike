28a71191e3374c1d307a8ceacc2ad7edc14aee22
AlexGoodD <alejandrochavarb@gmail.com>
2024-09-25 20:19:44 -0600

    Report function

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,6 +1,3 @@
-/*Esta es la pantalla principal que usa todos los componentes anteriores para mostrar el mapa,
-las rutas, y el widget de búsqueda.*/
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -10,8 +7,11 @@ import 'package:moonhike/domain/use_cases/get_routes_use_case.dart';
 import 'package:moonhike/core/widgets/address_search_widget.dart';
 import '../../core/utils/location_utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:moonhike/presentation/screens/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:moonhike/presentation/screens/login.dart';
+import 'dart:math' as math;
+import 'dart:async';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -27,9 +27,10 @@ class _MapScreenState extends State<MapScreen> {
   Set<Polyline> _polylines = {};
   int _selectedRouteIndex = 0;
   List<List<LatLng>> _routes = [];
-  String? _userEmail;  // Variable para almacenar el correo del usuario
+  String? _userEmail;
+  StreamSubscription<Position>? _positionStream;
 
-  final RouteRepository routeRepository = RouteRepository(RouteService('AIzaSyDNHOPdlWDOqsFiL9_UQCkg2fnlpyww6A4'));
+  final RouteRepository routeRepository = RouteRepository(RouteService('YOUR_GOOGLE_MAPS_API_KEY'));
   late GetRoutesUseCase getRoutesUseCase;
 
   _MapScreenState() {
@@ -39,8 +40,15 @@ class _MapScreenState extends State<MapScreen> {
   @override
   void initState() {
     super.initState();
-    _setInitialLocation();
-    _getUserEmail();  // Obtén el correo electrónico cuando la pantalla inicia
+    _getUserEmail(); // Obtén el correo electrónico del usuario
+    _startLocationUpdates(); // Inicia la escucha de las actualizaciones de ubicación
+  }
+
+  @override
+  void dispose() {
+    // Cancela el stream de ubicación al salir de la pantalla
+    _positionStream?.cancel();
+    super.dispose();
   }
 
   // Método para obtener el correo del usuario
@@ -51,42 +59,61 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
-  // Establece la ubicación inicial
-  void _setInitialLocation() async {
-    try {
-      Position position = await LocationUtils.getUserLocation();
-      print("Ubicación actual: ${position.latitude}, ${position.longitude}");
+  // Función para calcular la distancia entre dos puntos geográficos en metros
+  double _calculateDistance(LatLng start, LatLng end) {
+    const double earthRadius = 6371000; // Radio de la Tierra en metros
 
+    final lat1 = math.pi * start.latitude / 180;
+    final lat2 = math.pi * end.latitude / 180;
+    final lon1 = math.pi * start.longitude / 180;
+    final lon2 = math.pi * end.longitude / 180;
+
+    final dLat = lat2 - lat1;
+    final dLon = lon2 - lon1;
+
+    final a = math.sin(dLat / 2) * math.sin(dLat / 2) +
+        math.cos(lat1) * math.cos(lat2) * math.sin(dLon / 2) * math.sin(dLon / 2);
+
+    final c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a));
+
+    return earthRadius * c; // Resultado en metros
+  }
+
+  // Inicia la escucha de actualizaciones de ubicación
+  void _startLocationUpdates() {
+    _positionStream = Geolocator.getPositionStream(
+      locationSettings: LocationSettings(
+        accuracy: LocationAccuracy.high,  // Alta precisión para detectar movimientos pequeños
+        distanceFilter: 10,  // Actualizar cuando el usuario se mueva al menos 10 metros
+      ),
+    ).listen((Position position) {
+      // Actualiza la posición actual y refresca el mapa
       setState(() {
         _currentPosition = LatLng(position.latitude, position.longitude);
-        _markers.add(Marker(
-          markerId: MarkerId('currentLocation'),
-          position: _currentPosition!,
-          infoWindow: InfoWindow(title: 'Mi Ubicación'),
-        ));
+        _updateCurrentLocationMarker();
       });
 
-      _controller?.animateCamera(CameraUpdate.newLatLng(_currentPosition!));
-    } catch (e) {
-      print('Error obteniendo la ubicación: $e');
-      setState(() {
-        _currentPosition = LatLng(25.6866, -100.3161);  // Monterrey por defecto
-        _markers.add(Marker(
-          markerId: MarkerId('defaultLocation'),
-          position: _currentPosition!,
-          infoWindow: InfoWindow(title: 'Ubicación por Defecto: Monterrey'),
-        ));
-      });
+      // Mueve la cámara a la nueva posición del usuario
+      if (_controller != null) {
+        _controller?.animateCamera(CameraUpdate.newLatLng(_currentPosition!));
+      }
+    });
+  }
 
-      _controller?.animateCamera(CameraUpdate.newLatLng(_currentPosition!));
-    }
+  // Actualiza el marcador de la ubicación actual del usuario
+  void _updateCurrentLocationMarker() {
+    _markers.removeWhere((marker) => marker.markerId.value == 'currentLocation');
+    _markers.add(Marker(
+      markerId: MarkerId('currentLocation'),
+      position: _currentPosition!,
+      infoWindow: InfoWindow(title: 'Mi Ubicación'),
+    ));
   }
 
   // Función para cerrar sesión
   Future<void> _logout() async {
     await FirebaseAuth.instance.signOut();  // Cierra sesión en Firebase
 
-    // Elimina el UID de SharedPreferences
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove('userUID');
 
@@ -94,6 +121,89 @@ class _MapScreenState extends State<MapScreen> {
       context,
       MaterialPageRoute(builder: (context) => LoginPage()),
     );
+  }
+
+  // Función para cargar los reportes
+  Future<void> _loadReports() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference reports = firestore.collection('reports');
+
+    QuerySnapshot querySnapshot = await reports
+        .where('routeIndex', isEqualTo: _selectedRouteIndex)
+        .get();
+
+    const double proximityThreshold = 100.0; // Umbral de proximidad en metros
+
+    setState(() {
+      _markers.addAll(querySnapshot.docs.map((doc) {
+        GeoPoint location = doc['location'];
+        LatLng reportPosition = LatLng(location.latitude, location.longitude);
+
+        bool isNearRoute = _routes[_selectedRouteIndex].any((LatLng routePoint) {
+          double distance = _calculateDistance(reportPosition, routePoint);
+          return distance <= proximityThreshold;
+        });
+
+        if (isNearRoute) {
+          return Marker(
+            markerId: MarkerId('report_${doc.id}'),
+            position: reportPosition,
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+            infoWindow: InfoWindow(
+              title: 'Reporte de ${doc['user']}',
+              snippet: 'Generado el ${doc['timestamp'].toDate()}',
+            ),
+          );
+        }
+
+        return null;
+      }).whereType<Marker>().toList());
+    });
+  }
+
+  // Función para crear un reporte
+  Future<void> _createReport() async {
+    try {
+      if (_currentPosition == null) throw 'La ubicación actual no está disponible.';
+
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      CollectionReference reports = firestore.collection('reports');
+
+      // Crear el reporte en Firestore
+      await reports.add({
+        'user': _userEmail,
+        'location': GeoPoint(_currentPosition!.latitude, _currentPosition!.longitude),
+        'routeIndex': _selectedRouteIndex,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      setState(() {
+        final newMarker = Marker(
+          markerId: MarkerId('new_report_${DateTime.now().millisecondsSinceEpoch}'),
+          position: _currentPosition!,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          infoWindow: InfoWindow(
+            title: 'Tu Reporte',
+            snippet: 'Generado en tu ubicación actual',
+          ),
+        );
+        _markers.add(newMarker);
+
+        // Mover la cámara a la nueva ubicación del marcador
+        _controller?.animateCamera(CameraUpdate.newLatLngZoom(_currentPosition!, 16));
+
+        print("Marcador de reporte añadido en: $_currentPosition");
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Reporte creado exitosamente')),
+      );
+    } catch (e) {
+      print('Error al generar el reporte: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al generar el reporte: $e')),
+      );
+    }
   }
 
   // Inicia la búsqueda de rutas
@@ -129,12 +239,13 @@ class _MapScreenState extends State<MapScreen> {
         ));
       }
     });
+    _loadReports();  // Cargar reportes para la ruta seleccionada
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer( // Menú desplegable
+      drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
@@ -147,7 +258,7 @@ class _MapScreenState extends State<MapScreen> {
                 children: [
                   Text('Bienvenido', style: TextStyle(color: Colors.white, fontSize: 24)),
                   SizedBox(height: 10),
-                  if (_userEmail != null)  // Muestra el correo si está disponible
+                  if (_userEmail != null)
                     Text(_userEmail!, style: TextStyle(color: Colors.white, fontSize: 16)),
                 ],
               ),
@@ -163,7 +274,7 @@ class _MapScreenState extends State<MapScreen> {
               leading: Icon(Icons.logout),
               title: Text('Cerrar Sesión'),
               onTap: () {
-                _logout(); // Llama a la función de logout
+                _logout();
               },
             ),
           ],
@@ -175,7 +286,7 @@ class _MapScreenState extends State<MapScreen> {
           // Mapa de Google
           GoogleMap(
             initialCameraPosition: CameraPosition(
-              target: _currentPosition ?? LatLng(25.6866, -100.3161), // Monterrey por defecto
+              target: _currentPosition ?? LatLng(25.6866, -100.3161),
               zoom: 14.0,
             ),
             onMapCreated: (GoogleMapController controller) {
@@ -206,7 +317,6 @@ class _MapScreenState extends State<MapScreen> {
 
                   _controller?.animateCamera(CameraUpdate.newLatLng(_selectedLocation!));
 
-                  // Muestra el botón para iniciar rutas
                   _showStartRouteButton = true;
                 });
               },
@@ -223,6 +333,16 @@ class _MapScreenState extends State<MapScreen> {
                 child: Text('Iniciar Rutas'),
               ),
             ),
+          // Botón para crear un reporte
+          Positioned(
+            bottom: 50,
+            left: 10,
+            right: 10,
+            child: ElevatedButton(
+              onPressed: _createReport,
+              child: Text('Generar Reporte'),
+            ),
+          ),
           // Opciones de selección de rutas
           if (_routes.isNotEmpty)
             Positioned(
